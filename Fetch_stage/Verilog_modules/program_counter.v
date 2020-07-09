b8dc3b0cdbb8eefab79669a2a2d066523b1f4237
JagrutJadhav <64086840+JagrutJadhav@users.noreply.github.com>
2020-07-09 16:35:56 +0530

    Update program_counter.v

--- Fetch_stage/Verilog_modules/program_counter.v
+++ Fetch_stage/Verilog_modules/program_counter.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Engineer: Jagrut Jadhav
 // Create Date: 06/30/2020 05:57:13 PM
@@ -8,13 +8,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module program_counter( input clk,
+module Program_counter_2( input clk,
 input CE,
 input PC_rd,PC_wr,//// low signal => write counter address to counter , High => read counter address
 input [4:0] addressin,
+input reset,
 output [4:0] addressout
  );
- reg [4:0] i=5'b00000;   
+ reg [4:0] i= 5'b00000;   
  
  always @ (posedge clk) begin
     if (CE)begin
@@ -24,7 +25,11 @@ output [4:0] addressout
         else begin
             i <= i+1;
         end
-    end 
+    end
+    
+    else if (reset) i <= 5'b00000;
+    else i<= i;
+     
  end  
- assign addressout = i;
+ assign addressout = (PC_rd)?i:5'dz;
 endmodule
